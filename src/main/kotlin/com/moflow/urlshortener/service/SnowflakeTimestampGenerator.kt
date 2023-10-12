4af21a587a2ac0e2d7a4a15e47e9f86e83a17f82
moflow <hojihun5516@daum.net>
2023-10-12 23:10:48 +0900

    snowflake - timestamp 영역 생성
     close: #15

---
+++ src/main/kotlin/com/moflow/urlshortener/service/SnowflakeTimestampGenerator.kt
@@ -0,0 +1,19 @@
+package com.moflow.urlshortener.service
+
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+@Component
+class SnowflakeTimestampGenerator {
+
+    fun currentTimestamp(): Long {
+        val bitOperand = ((1L shl TIMESTAMP_BITS) - 1)
+        val timeMillis = Instant.now().toEpochMilli()
+        return (timeMillis - EPOCH) and bitOperand //and 연산자를 통해 필요한 41비트만 사용
+    }
+
+    companion object {
+        private const val EPOCH = 1609459200000L // 2021-01-01T00:00:00Z
+        private const val TIMESTAMP_BITS = 41
+    }
+}
---
+++ src/test/kotlin/com/moflow/urlshortener/service/SnowflakeTimestampGeneratorTest.kt
@@ -0,0 +1,36 @@
+package com.moflow.urlshortener.service
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockkStatic
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.Instant
+
+@ExtendWith(MockKExtension::class)
+class SnowflakeTimestampGeneratorTest {
+
+    @InjectMockKs
+    private lateinit var sut: SnowflakeTimestampGenerator
+
+    @Test
+    fun `sut should generate 41 bits timestamp`() {
+        // Arrange
+        val instant = Instant.parse("2021-08-20T00:00:00Z")
+        val currentMillis = instant.toEpochMilli() // 2021-08-20T00:00:00Z
+        val epoch = 1609459200000L
+        val timestampBits = 41
+        mockkStatic(Instant::class)
+        every { Instant.now() } returns instant
+
+        val expected = (currentMillis - epoch) and ((1L shl timestampBits) - 1)
+
+        // Act
+        val actual = sut.currentTimestamp()
+
+        // Assert
+        assertThat(actual).isEqualTo(expected)
+    }
+}
