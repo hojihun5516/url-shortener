97dec76fa7aa3f1429e1bec3fe5dbaaa9eb8d9b4
moflow <hojihun5516@daum.net>
2023-11-04 18:42:29 +0900

    SnowflakeTimestampGenerator return 타입 string으로 변경
    * 식별자 용도로 41비트를 표현할 때 Long보다는 String이 더 낫다고 판단
    
    close: #23

--- src/main/kotlin/com/moflow/urlshortener/service/SnowflakeTimestampGenerator.kt
+++ src/main/kotlin/com/moflow/urlshortener/service/SnowflakeTimestampGenerator.kt
@@ -6,10 +6,10 @@ import java.time.Instant
 @Component
 class SnowflakeTimestampGenerator {
 
-    fun currentTimestamp(): Long {
+    fun currentTimestamp(): String {
         val bitOperand = ((1L shl TIMESTAMP_BITS) - 1)
         val timeMillis = Instant.now().toEpochMilli()
-        return (timeMillis - EPOCH) and bitOperand //and 연산자를 통해 필요한 41비트만 사용
+        return ((timeMillis - EPOCH) and bitOperand).toString() //and 연산자를 통해 필요한 41비트만 사용
     }
 
     companion object {
