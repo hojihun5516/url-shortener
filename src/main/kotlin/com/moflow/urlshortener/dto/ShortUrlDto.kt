5a8ba1056e5c1b1f3774a87fac3a413353cbf233
moflow <hojihun5516@daum.net>
2023-11-06 23:43:47 +0900

    ShortUrl Dto 생성
    close: #32

---
+++ src/main/kotlin/com/moflow/urlshortener/dto/ShortUrlDto.kt
@@ -0,0 +1,6 @@
+package com.moflow.urlshortener.dto
+
+class ShortUrlDto(
+    val originUrl: String,
+    val shortUrl: String,
+)
---
+++ src/main/kotlin/com/moflow/urlshortener/extension/ShortUrlExtension.kt
@@ -0,0 +1,13 @@
+package com.moflow.urlshortener.extension
+
+import com.moflow.urlshortener.domain.ShortUrl
+import com.moflow.urlshortener.dto.ShortUrlDto
+
+object ShortUrlExtension {
+    fun ShortUrl.toShortUrlDto(): ShortUrlDto {
+        return ShortUrlDto(
+            originUrl = originUrl,
+            shortUrl = shortUrl,
+        )
+    }
+}
