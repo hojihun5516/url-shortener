b316a83f171bafaea1df1588bbb9c60f19e72270
moflow <hojihun5516@daum.net>
2023-11-08 17:57:48 +0900

    ShortUrlCreateRequest - URL validation annotation 사용
    close: #41

--- src/main/kotlin/com/moflow/urlshortener/dto/ShortUrlCreateRequest.kt
+++ src/main/kotlin/com/moflow/urlshortener/dto/ShortUrlCreateRequest.kt
@@ -1,5 +1,10 @@
 package com.moflow.urlshortener.dto
 
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.URL
+
 data class ShortUrlCreateRequest(
+    @field:NotBlank(message = "The URL must not be blank")
+    @field:URL(message = "Invalid URL format")
     val originUrl: String,
 )
