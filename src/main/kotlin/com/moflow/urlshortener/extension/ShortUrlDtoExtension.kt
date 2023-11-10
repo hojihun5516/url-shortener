fff1ab12eccde31bd283cde6ce21bce17891712a
moflow <hojihun5516@daum.net>
2023-11-10 17:50:34 +0900

    Short url controller에 service 로직 사용
    close: #43

---
+++ src/main/kotlin/com/moflow/urlshortener/extension/ShortUrlDtoExtension.kt
@@ -0,0 +1,12 @@
+package com.moflow.urlshortener.extension
+
+import com.moflow.urlshortener.dto.ShortUrlCreateResponse
+import com.moflow.urlshortener.dto.ShortUrlDto
+
+object ShortUrlDtoExtension {
+    fun ShortUrlDto.toShortUrlCreateResponse(): ShortUrlCreateResponse {
+        return ShortUrlCreateResponse(
+            shortUrl = shortUrl,
+        )
+    }
+}
