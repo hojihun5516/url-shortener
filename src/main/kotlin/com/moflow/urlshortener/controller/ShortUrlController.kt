9c11efab26e707bdde14763d3e6177158110b4ea
moflow <hojihun5516@daum.net>
2023-11-10 17:50:34 +0900

    Short url controller에 service 로직 사용
    close: #43

--- src/main/kotlin/com/moflow/urlshortener/controller/ShortUrlController.kt
+++ src/main/kotlin/com/moflow/urlshortener/controller/ShortUrlController.kt
@@ -2,16 +2,23 @@ package com.moflow.urlshortener.controller
 
 import com.moflow.urlshortener.dto.ShortUrlCreateRequest
 import com.moflow.urlshortener.dto.ShortUrlCreateResponse
+import com.moflow.urlshortener.extension.ShortUrlDtoExtension.toShortUrlCreateResponse
+import com.moflow.urlshortener.service.ShortUrlMappingService
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ShortUrlController {
-
+class ShortUrlController(
+    private val shortUrlMappingService: ShortUrlMappingService
+) {
     @PostMapping("/short-url")
-    fun shortUrl(@RequestBody @Validated shortUrlCreateRequest: ShortUrlCreateRequest): ShortUrlCreateResponse {
-        return ShortUrlCreateResponse("tempShortUrl")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun map(@RequestBody @Validated shortUrlCreateRequest: ShortUrlCreateRequest): ShortUrlCreateResponse {
+        val shortUrlDto = shortUrlMappingService.mapShortUrl(shortUrlCreateRequest.originUrl)
+        return shortUrlDto.toShortUrlCreateResponse()
     }
 }
--- src/test/kotlin/com/moflow/urlshortener/controller/ShortUrlControllerTest.kt
+++ src/test/kotlin/com/moflow/urlshortener/controller/ShortUrlControllerTest.kt
@@ -3,6 +3,11 @@ package com.moflow.urlshortener.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.moflow.urlshortener.dto.ShortUrlCreateRequest
 import com.moflow.urlshortener.dto.ShortUrlCreateResponse
+import com.moflow.urlshortener.dto.ShortUrlDto
+import com.moflow.urlshortener.service.ShortUrlMappingService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class ShortUrlControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
     private val objectMapper: ObjectMapper,
+    @MockkBean private val shortUrlMappingService: ShortUrlMappingService,
 ) {
 
     @Test
@@ -27,8 +33,12 @@ class ShortUrlControllerTest @Autowired constructor(
     fun `sut return short url when origin url is given`() {
         // Arrange
         val originUrl = "https://google.com"
+        val shortUrl = "asdiew2Z"
+        val shortUrlDto = ShortUrlDto(originUrl, shortUrl)
         val shortUrlCreateRequest = ShortUrlCreateRequest(originUrl)
-        val shortUrlCreateResponse = ShortUrlCreateResponse("tempShortUrl")
+        val shortUrlCreateResponse = ShortUrlCreateResponse(shortUrl)
+
+        every { shortUrlMappingService.mapShortUrl(originUrl) } returns shortUrlDto
 
         // Act & Assert
         mockMvc.perform(
@@ -37,7 +47,28 @@ class ShortUrlControllerTest @Autowired constructor(
                 .content(objectMapper.writeValueAsString(shortUrlCreateRequest))
         )
             .andDo(print())
-            .andExpect(status().isOk)
+            .andExpect(status().isCreated)
             .andExpect(jsonPath("$.shortUrl").value(shortUrlCreateResponse.shortUrl))
+
+        verify {
+            shortUrlMappingService.mapShortUrl(originUrl)
+        }
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 url은 에러를 발생시킨다")
+    fun `sut should error url when url is not valid`() {
+        // Arrange
+        val originUrl = "asdfhttps://google.com"
+        val shortUrlCreateRequest = ShortUrlCreateRequest(originUrl)
+
+        // Act & Assert
+        mockMvc.perform(
+            post("/short-url")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(shortUrlCreateRequest))
+        )
+            .andDo(print())
+            .andExpect(status().is4xxClientError)
     }
 }
