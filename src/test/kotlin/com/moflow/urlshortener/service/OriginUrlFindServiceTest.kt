ed7aa955f0c48260abedc1a2efbc78d53684ab19
moflow <hojihun5516@daum.net>
2023-11-12 21:32:30 +0900

    Origin url을 찾을 때 캐시를 설정한다
    * Origin url에 대한 Redis Cache는 5일을 기본으로 한다
    
    close: #52

--- src/test/kotlin/com/moflow/urlshortener/service/OriginUrlFindServiceTest.kt
+++ src/test/kotlin/com/moflow/urlshortener/service/OriginUrlFindServiceTest.kt
@@ -1,34 +1,66 @@
 package com.moflow.urlshortener.service
 
+import com.moflow.urlshortener.cache.ManagedCache
 import com.moflow.urlshortener.domain.ShortUrl
 import com.moflow.urlshortener.exception.Messages.ORIGIN_URL_NOT_FOUND_BY_SHORT_URL
 import com.moflow.urlshortener.exception.NotFoundException
 import com.moflow.urlshortener.repository.ShortUrlRepository
+import io.mockk.Called
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.justRun
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.TimeUnit
 
 @ExtendWith(MockKExtension::class)
 class OriginUrlFindServiceTest(
     @MockK private val shortUrlRepository: ShortUrlRepository,
+    @MockK private val managedCache: ManagedCache,
 ) {
     @InjectMockKs
     private lateinit var sut: OriginUrlFindService
 
     @Test
-    fun `sut should return short url dto when short url is given`() {
+    @DisplayName("캐시에 데이터가 있다면, short url dto를 반환한다")
+    fun `sut should return short url dto when short url is given and cache is exists`() {
         // Arrange
         val originUrl = "https://ASDF.com"
         val shortenUrl = "asdfxC2"
+        val redisKey = "short-url:$shortenUrl"
+        every { managedCache.get<String>(redisKey) } returns originUrl
+
+        // Act
+        val actual = sut.findByShortUrl(shortenUrl)
+
+        // Assert
+        assertThat(actual)
+            .hasFieldOrPropertyWithValue("originUrl", originUrl)
+            .hasFieldOrPropertyWithValue("shortUrl", shortenUrl)
+
+        verify {
+            shortUrlRepository wasNot Called
+            managedCache.get<String>(redisKey)
+        }
+    }
+
+    @Test
+    @DisplayName("캐시에 데이터가 없고, 등록된 short url이 있다면 short url dto를 반환한다")
+    fun `sut should return short url dto when short url is given and cache is not exists`() {
+        // Arrange
+        val originUrl = "https://ASDF.com"
+        val shortenUrl = "asdfxC2"
+        val redisKey = "short-url:$shortenUrl"
         val shortUrl = ShortUrl(originUrl = originUrl, shortUrl = shortenUrl)
+        every { managedCache.get<String>(redisKey) } returns null
         every { shortUrlRepository.findByShortUrl(shortenUrl) } returns shortUrl
+        justRun { managedCache.set(redisKey, originUrl, 5L, TimeUnit.DAYS) }
 
         // Act
         val actual = sut.findByShortUrl(shortenUrl)
@@ -40,13 +72,19 @@ class OriginUrlFindServiceTest(
 
         verify {
             shortUrlRepository.findByShortUrl(shortenUrl)
+            managedCache.get<String>(redisKey)
+            managedCache.set(redisKey, originUrl, 5L, TimeUnit.DAYS)
         }
     }
 
     @Test
-    fun `sut should return null when short url is not exists`() {
+    @DisplayName("캐시에 데이터가 없고, 등록된 short url이 없다면 Exception을 발생시킨다")
+    fun `sut throw not found exception when short url is not exists`() {
         // Arrange
-        val shortenUrl = "asdklxc"
+        val shortenUrl = "asdfxC2"
+        val redisKey = "short-url:$shortenUrl"
+
+        every { managedCache.get<String>(redisKey) } returns null
         every { shortUrlRepository.findByShortUrl(shortenUrl) } returns null
 
         // Act & Assert
@@ -56,6 +94,7 @@ class OriginUrlFindServiceTest(
         assertThat(exception.code).isEqualTo(ORIGIN_URL_NOT_FOUND_BY_SHORT_URL.first)
         verify {
             shortUrlRepository.findByShortUrl(shortenUrl)
+            managedCache.get<String>(redisKey)
         }
     }
 }
