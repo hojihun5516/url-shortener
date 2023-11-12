e432009ea50fd08a28a3054a0f6d788cb5f3f963
moflow <hojihun5516@daum.net>
2023-11-12 21:32:30 +0900

    Origin url을 찾을 때 캐시를 설정한다
    * Origin url에 대한 Redis Cache는 5일을 기본으로 한다
    
    close: #52

--- src/main/kotlin/com/moflow/urlshortener/service/OriginUrlFindService.kt
+++ src/main/kotlin/com/moflow/urlshortener/service/OriginUrlFindService.kt
@@ -1,5 +1,6 @@
 package com.moflow.urlshortener.service
 
+import com.moflow.urlshortener.cache.ManagedCache
 import com.moflow.urlshortener.dto.ShortUrlDto
 import com.moflow.urlshortener.exception.Messages.ORIGIN_URL_NOT_FOUND_BY_SHORT_URL
 import com.moflow.urlshortener.exception.NotFoundException
@@ -7,14 +8,28 @@ import com.moflow.urlshortener.extension.ShortUrlExtension.toShortUrlDto
 import com.moflow.urlshortener.repository.ShortUrlRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
 
 @Service
 @Transactional(readOnly = true)
 class OriginUrlFindService(
     private val shortUrlRepository: ShortUrlRepository,
+    private val managedCache: ManagedCache,
 ) {
     fun findByShortUrl(shortUrl: String): ShortUrlDto {
-        return shortUrlRepository.findByShortUrl(shortUrl)?.toShortUrlDto()
+        val redisKey = REDIS_KEY_PREFIX + shortUrl
+        managedCache.get<String>(redisKey)?.let {
+            return ShortUrlDto(originUrl = it, shortUrl = shortUrl)
+        }
+
+        val shortUrlDto = shortUrlRepository.findByShortUrl(shortUrl)?.toShortUrlDto()
             ?: throw NotFoundException(ORIGIN_URL_NOT_FOUND_BY_SHORT_URL, shortUrl)
+        managedCache.set(redisKey, shortUrlDto.originUrl, REDIS_CACHE_TTL, TimeUnit.DAYS)
+        return shortUrlDto
+    }
+
+    companion object {
+        private const val REDIS_KEY_PREFIX = "short-url:"
+        private const val REDIS_CACHE_TTL = 5L
     }
 }
