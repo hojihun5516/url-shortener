6addedeedb83bb52825cc075f34fa2502b77fcb8
moflow <hojihun5516@daum.net>
2023-11-12 19:11:13 +0900

    Redirect controller 기능 개발
     close: #48

--- src/main/kotlin/com/moflow/urlshortener/controller/ShortUrlController.kt
+++ src/main/kotlin/com/moflow/urlshortener/controller/ShortUrlController.kt
@@ -6,10 +6,16 @@ import com.moflow.urlshortener.extension.ShortUrlDtoExtension.toShortUrlCreateRe
 import com.moflow.urlshortener.service.ShortUrlMappingService
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+
+
 
 @RestController
 class ShortUrlController(
@@ -20,5 +26,17 @@ class ShortUrlController(
     fun map(@RequestBody @Validated shortUrlCreateRequest: ShortUrlCreateRequest): ShortUrlCreateResponse {
         val shortUrlDto = shortUrlMappingService.mapShortUrl(shortUrlCreateRequest.originUrl)
         return shortUrlDto.toShortUrlCreateResponse()
+    }
+
+    /**
+     * TODO
+     * Service 구현 후 url 변경
+     */
+    @GetMapping("/redirect/{shortUrl}")
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    fun redirect(@PathVariable shortUrl: String): RedirectView {
+        val redirectView = RedirectView()
+        redirectView.url = "https://www.naver.com"
+        return redirectView;
     }
 }
