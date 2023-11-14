65e59eb0ec60098529bb14ebd99e0e0be3b5252e
moflow <hojihun5516@daum.net>
2023-11-14 20:13:22 +0900

    docs shorten_url 스키마 변경
    * short_key로 변경
    * origin_url unique 등록
    
    close: #66

--- docs/schema/create-table-shorten_url.sql
+++ docs/schema/create-table-shorten_url.sql
@@ -3,9 +3,10 @@ create table shorten_url
     id             bigint unsigned auto_increment
         primary key,
     original_url   text    not null  comment '원본 URL',
-    short_url      varchar(100)     null comment '단축 URL',
+    short_key      varchar(31)     null comment '단축 Key',
     created_at     datetime         not null default CURRENT_TIMESTAMP,
     updated_at     datetime         not null default CURRENT_TIMESTAMP,
 
-    constraint shorten_url_short_url_uindex unique (short_url)
+    constraint shorten_url_origin_url_uindex unique (origin_url),
+    constraint shorten_url_short_key_uindex unique (short_key)
 );
