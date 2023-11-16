f9125c768a4fcc0b9e64b51ca84e757f39477de3
moflow <hojihun5516@daum.net>
2023-11-16 17:07:03 +0900

    ShortUrlFindService에 cache를 사용하여 부하를 줄인다
    * 같은 url에 대해서 맵핑 중복 요청은 최대 3일정도가 소요될것으로 예상하여 Read Through 캐시사용 (행사 등)
    
    close: #73

--- src/main/kotlin/com/moflow/urlshortener/service/ShortUrlFindService.kt
+++ src/main/kotlin/com/moflow/urlshortener/service/ShortUrlFindService.kt
@@ -1,17 +1,33 @@
 package com.moflow.urlshortener.service
 
+import com.moflow.urlshortener.cache.ManagedCache
 import com.moflow.urlshortener.dto.ShortUrlDto
 import com.moflow.urlshortener.extension.ShortUrlExtension.toShortUrlDto
 import com.moflow.urlshortener.repository.ShortUrlRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
 
 @Service
 @Transactional(readOnly = true)
 class ShortUrlFindService(
     private val shortUrlRepository: ShortUrlRepository,
+    private val managedCache: ManagedCache,
 ) {
     fun findByOriginUrl(originUrl: String): ShortUrlDto? {
-        return shortUrlRepository.findByOriginUrl(originUrl)?.toShortUrlDto()
+        val redisKey = REDIS_KEY_PREFIX + originUrl
+        managedCache.get<String>(redisKey)?.let {
+            return ShortUrlDto(originUrl = originUrl, shortKey = it)
+        }
+
+        return shortUrlRepository.findByOriginUrl(originUrl)?.let {
+            managedCache.set(redisKey, it.shortKey, REDIS_CACHE_TTL, TimeUnit.DAYS)
+            return it.toShortUrlDto()
+        }
+    }
+
+    companion object {
+        private const val REDIS_KEY_PREFIX = "origin-url:"
+        private const val REDIS_CACHE_TTL = 3L
     }
 }
